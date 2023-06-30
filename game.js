0037c47f9b2980f9d898db998e25d9936a58bdf9
Jason <github@jasongetsemail.com>
2023-06-30 14:12:21 -0400

    Refactored nextSequence() to allow for the newly created playSound() to still play a sound when a new colour is chosen. Also added logic to playSound() to play a sound whenever a button is clicked by the user

--- game.js
+++ game.js
@@ -1,23 +1,28 @@
 let buttonColours = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let userClickedPattern = [];
+
 function nextSequence() {
   let randomNumber = Math.round(Math.random() * 3);
-  return randomNumber;
+  let randomChosenColour = buttonColours[randomNumber];
+  let toFlash = $("#" + randomChosenColour);
+  toFlash.fadeOut(50).fadeIn(50);
+  playSound(randomChosenColour);
 }
 
-let randomChosenColour = buttonColours[nextSequence()];
+function playSound(name) {
+  let audio = new Audio(
+    "Reference Files/Simon Game Challenge Starting Files/sounds/" +
+      name +
+      ".mp3"
+  );
+  audio.play();
+}
 
-let toFlash = $("#" + randomChosenColour);
-let audio = new Audio(
-  "Reference Files/Simon Game Challenge Starting Files/sounds/" +
-    randomChosenColour +
-    ".mp3"
-);
-toFlash.fadeOut(50).fadeIn(50);
-audio.play();
+nextSequence();
 
 $(".btn").click(function (event) {
   let userChosenColour = event.target.id;
   userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
 });

1518c1471592feac06b288db1d2febe42f1ada52
Jason <github@jasongetsemail.com>
2023-06-30 20:51:27 -0400

    Added answer checking logic

--- game.js
+++ game.js
@@ -1,12 +1,17 @@
 let buttonColours = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let userClickedPattern = [];
+let level = 0;
+let correctAnswers = 0;
 
 function nextSequence() {
+  level++;
+  $("h1").html(level);
   let randomNumber = Math.round(Math.random() * 3);
   let randomChosenColour = buttonColours[randomNumber];
   let toFlash = $("#" + randomChosenColour);
   toFlash.fadeOut(50).fadeIn(50);
+  gamePattern.push(randomChosenColour);
   playSound(randomChosenColour);
 }
 
@@ -27,12 +32,32 @@ function animatePress(currentColour) {
   }, 100);
 }
 
-nextSequence();
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] == gamePattern[currentLevel]) {
+    console.log("yes");
+    correctAnswers++;
+  } else {
+    console.log("wrong");
+  }
+}
 
 $(".btn").click(function (event) {
-  console.log(event);
   let userChosenColour = event.target.id;
   userClickedPattern.push(userChosenColour);
   playSound(userChosenColour);
   animatePress(userChosenColour);
+  checkAnswer(correctAnswers);
+  if (gamePattern.length == userClickedPattern.length) {
+    userClickedPattern = [];
+    correctAnswers = 0;
+    setTimeout(function () {
+      nextSequence();
+    }, 1000);
+  }
+});
+
+$("html").keydown(function () {
+  if (level == 0) {
+    nextSequence();
+  }
 });
